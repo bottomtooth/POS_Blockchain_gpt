2d36b95805af3615d6861d7fc788977a7682c128
bottomtooth <34635778+bottomtooth@users.noreply.github.com>
2023-05-15 15:08:06 +0100

    Delete blockchain_web directory

--- blockchain_web/blockchain_app/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class TransactionForm(forms.Form):
-    sender = forms.CharField(label='Sender', max_length=100)
-    recipient = forms.CharField(label='Recipient', max_length=100)
-    amount = forms.DecimalField(label='Amount')
